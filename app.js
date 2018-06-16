9a36bc3585ef794c597230be9575f0879e8e29cf
Yetsuh <Yetsuh@Yetsuhs-MacBook-Air.local>
2018-06-16 13:45:19 -0400

    a

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 var express = require('express')
 var app = express()
 
-var express = require('express');
-//var server = express.createServer();
-// express.createServer()  is deprecated. 
-var server = express(); // better instead
-server.configure(function(){
-  server.use(express.static(__dirname + '/public'));
-});
+app.use('/', express.static('public'));
 
 server.listen(9000);
